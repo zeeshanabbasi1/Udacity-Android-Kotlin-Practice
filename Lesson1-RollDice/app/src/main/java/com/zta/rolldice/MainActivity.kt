5ba7bcd573f6e59d7526c69a6bc480aa2d51a3d5
Zeeshan <zeeshan.abbasi08@gmail.com>
2018-11-10 19:13:13 +0500

    Rename to Lesson1-RollDice

---
+++ Lesson1-RollDice/app/src/main/java/com/zta/rolldice/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.zta.rolldice
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+    lateinit var rollButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        rollButton = findViewById(R.id.roll_btn)
+        diceImage= findViewById(R.id.dice_image)
+
+
+        rollButton.setOnClickListener {
+
+            val randomInt = Random().nextInt(6) + 1
+            val drwableResouce = when (randomInt) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+
+
+            val diceImage: ImageView = findViewById(R.id.dice_image)
+            diceImage.setImageResource(drwableResouce)
+
+
+        }
+    }
+}
